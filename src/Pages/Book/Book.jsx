f06daa1a3ace500743f8e1e99b3ffc107b5eae4d
Asad Zaman <10asadzaman@gmail.com>
2025-11-08 19:32:17 +0600

    implemented dynamic route

--- src/Pages/Book/Book.jsx
+++ src/Pages/Book/Book.jsx
@@ -4,12 +4,12 @@ import { FaStarHalf } from 'react-icons/fa';
 
 
 const Book = ({ singleBook }) => {
-    const { bookId, bookName, category, image, publisher, rating, author, review, totalPages, yearOfPublishing } = singleBook;
+    const { bookId, bookName, category, image, publisher, rating, author, review, totalPages, yearOfPublishing, tags} = singleBook;
     console.log(singleBook);
     return (
         <div >
             <div className="card bg-base-100 w-96 shadow-sm border-2 border-black rounded-2xl">
-                <figure>
+                <figure className='bg-gray-100 p-3 w-2/3 mx-auto'>
                     <img className='w-[134px]'
                         src={image}
                         alt="Shoes" />
@@ -17,15 +17,21 @@ const Book = ({ singleBook }) => {
                 <div className="card-body">
                     <h2 className="card-title">
                        {bookName}
-                        <div className="badge badge-primary">{author}</div>
+                        <div >
+                             {tags.map(tag => <div className="badge badge-primary m-1">{tag}</div>)}
+                        </div>
                     </h2>
-                    <p>{review}</p>
+                    <h2>Book by: {author}</h2>
+                    <h2>Year of Publishing: {yearOfPublishing}</h2>
+                    <h2>Category: {category}</h2>
+                     <div className="badge badge-outline">Total Pages: {totalPages}</div>
+                     <div className='border-1 border-dashed'></div>
+         
                     <div className="card-actions justify-end">
-                        <div className="badge badge-outline">Publisher: {publisher}</div>
+                
                         <div className="badge badge-outline">Rating: {rating} <CiStar></CiStar></div>
-                        <div className="badge badge-outline">Category: {category}</div>
-                        <div className="badge badge-outline">Year of Publishing: {yearOfPublishing}</div>
-                        <div className="badge badge-outline">Total Pages: {totalPages}</div>
+                                           
+                       
                     </div>
                 </div>
             </div>
